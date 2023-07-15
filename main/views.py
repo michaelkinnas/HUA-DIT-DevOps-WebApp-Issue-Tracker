cafef94eac47cf49e086dbf40b37ebaa49e23ea0
Michael Kinnas <michaelkinnas@gmail.com>
2023-07-15 18:07:38 +0300

    added static files icon

--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 import datetime
 from .models import Issue, Project
 from .forms import createNewIssue, createNewProject
