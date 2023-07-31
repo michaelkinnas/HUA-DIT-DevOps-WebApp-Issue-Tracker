2bb3cf57f64dff6df44e8e5e6b9d5a6bf042b52b
Michael Kinnas <michaelkinnas@gmail.com>
2023-07-31 11:33:54 +0300

    restored email vars because of some bug

--- issue_tracker/settings.py
+++ issue_tracker/settings.py
@@ -146,8 +146,8 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = env("EMAIL_HOST")
-EMAIL_PORT = env("EMAIL_PORT")
+EMAIL_HOST = '192.168.56.131'
+EMAIL_PORT = 1025
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
