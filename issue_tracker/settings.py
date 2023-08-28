eafa3159324465267e5fe9a32c5844a578dd475b
Michael Kinnas <michaelkinnas@gmail.com>
2023-08-28 16:03:50 +0300

    fixed bug with creation of default users

--- issue_tracker/settings.py
+++ issue_tracker/settings.py
@@ -84,18 +84,18 @@ WSGI_APPLICATION = 'issue_tracker.wsgi.application'
 
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': env("DB_NAME"),
-    #     'USER': env("DB_USERNAME"),
-    #     'PASSWORD': env("DB_PASSWORD"),
-    #     'HOST': env("DB_HOST"),
-    #     'PORT': env("DB_PORT"),
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env("DB_NAME"),
+        'USER': env("DB_USERNAME"),
+        'PASSWORD': env("DB_PASSWORD"),
+        'HOST': env("DB_HOST"),
+        'PORT': env("DB_PORT"),
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 # Password validation
--- main/signals.py
+++ main/signals.py
@@ -21,19 +21,21 @@ def createTestUsers(sender, **kwargs):
         )
     environ.Env.read_env()
 
-
-    developer1 = User.objects.create_user(env('DJANGO_DEV_NAME'), env('DJANGO_DEV_EMAIL'), env('DJANGO_DEV_PASSWORD'))
-    if not developer1:
-        raise Exception("Error creating user")
+    # if not User.objects.get(username=env('DJANGO_DEV_NAME')):
+    if not User.objects.filter(username=env('DJANGO_DEV_NAME')).exists():
+        developer1 = User.objects.create_user(env('DJANGO_DEV_NAME'), env('DJANGO_DEV_EMAIL'), env('DJANGO_DEV_PASSWORD'))
+        if not developer1:
+            raise Exception("Error creating user")
     
-    group = Group.objects.get(name='developer')
-    developer1.groups.add(group)
+        group = Group.objects.get(name='developer')
+        developer1.groups.add(group)
 
-    leader1 = User.objects.create_user(env('DJANGO_LEAD_NAME'), env('DJANGO_LEAD_EMAIL'), env('DJANGO_LEAD_PASSWORD'))
-    if not developer1:
-        raise Exception("Error creating user")
+    if not User.objects.filter(username=env('DJANGO_LEAD_NAME')).exists():
+        leader1 = User.objects.create_user(env('DJANGO_LEAD_NAME'), env('DJANGO_LEAD_EMAIL'), env('DJANGO_LEAD_PASSWORD'))
+        if not leader1:
+            raise Exception("Error creating user")
     
-    group = Group.objects.get(name='developer')
-    leader1.groups.add(group)
-    group = Group.objects.get(name='team_leader')
-    leader1.groups.add(group)
+        group = Group.objects.get(name='developer')
+        leader1.groups.add(group)
+        group = Group.objects.get(name='team_leader')
+        leader1.groups.add(group)
